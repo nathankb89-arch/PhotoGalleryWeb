e797da9aa3cd8b514e2e4ee2ca50c3f76c5a00e2
nate <nathankb89@gmail.com>
2026-07-11 21:53:37 +0300

    Implement user profile management with signals and environment configuration

--- Photo_App/apps.py
+++ Photo_App/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class PhotoAppConfig(AppConfig):
-    name = 'Photo_App'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "Photo_App"
+
+    def ready(self):
+        import Photo_App.signals
--- Photo_App/models.py
+++ Photo_App/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(
+        upload_to='profiles/',
+        default='default.jpg'
+    )
+
+    def __str__(self):
+        return self.user.username
