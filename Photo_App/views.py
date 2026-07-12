22af99b33f9396b23cd3f80c1f22bbfa39b9c230
nate <nathankb89@gmail.com>
2026-07-12 11:19:25 +0300

    Add user registration view and template with login redirect

--- Photo_App/views.py
+++ Photo_App/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .forms import RegisterForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def register(request):
+
+    if request.method == "POST":
+
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+
+            form.save()
+
+            return redirect("login")
+
+    else:
+
+        form = RegisterForm()
+
+    return render(
+        request,
+        "registration/register.html",
+        {
+            "form": form
+        }
+    )
--- Photo_Project/urls.py
+++ Photo_Project/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+    "login/",
+    LoginView.as_view(),
+    name="login"
+)
 ]
